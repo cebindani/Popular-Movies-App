aa436cf44f3873db3a6914e649effb478a9868b3
Daniele Cebin <danielecebin@hotmail.com>
2016-04-22 17:07:36 -0300

    Set ImageView with Picasso

--- app/src/main/java/com/cebin/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/cebin/popularmoviesapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.cebin.popularmoviesapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String base_url = "http://image.tmdb.org/t/p/";
+        String imgSize = "w185";
+        String poster = "/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg";
+
+
+        ImageView imageView = (ImageView) findViewById(R.id.banner_imageView);
+
+        Picasso.with(this)
+                .setIndicatorsEnabled(true);
+        Picasso.with(this)
+                .load(base_url+imgSize+poster)
+                .into(imageView);
+
+
+
     }
 }
