3ba7b860702c2210c103af62329556815561dbe4
Daniele Cebin <danielecebin@hotmail.com>
2016-04-28 22:54:18 -0300

    Movie implements Parcelable

--- app/src/main/java/com/cebin/popularmoviesapp/Movie.java
+++ app/src/main/java/com/cebin/popularmoviesapp/Movie.java
@@ -1,12 +1,25 @@
 package com.cebin.popularmoviesapp;
 
 import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by DanieleM on 23/04/2016.
  */
-public class Movie {
+public class Movie implements Parcelable {
 
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
     long id;
     String posterPath = null;
     String originalTitle = null;
@@ -15,10 +28,21 @@ public class Movie {
     Double userRating = 0.0; //vote_average
     String releaseDate = null;
 
+
     public Movie() {
         super();
     }
 
+
+    public Movie(Parcel in) {
+        id = in.readLong();
+        originalTitle = in.readString();
+        posterThumbnail = in.readString();
+        synopsis = in.readString();
+        userRating = in.readDouble();
+        releaseDate = in.readString();
+
+    }
 
     public String getPosterURL() {
 
@@ -37,5 +61,19 @@ public class Movie {
 
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(originalTitle);
+        dest.writeString(posterThumbnail);
+        dest.writeString(synopsis);
+        dest.writeDouble(userRating);
+        dest.writeString(releaseDate);
+
+    }
 }
